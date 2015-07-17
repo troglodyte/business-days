51db11521b58458bb896cf45464e0947c1c6588d
Michael Harris <mtharris@ithaca.edu>
2015-07-17 14:46:22 -0400

    removing tests that relied on datetime(now)

--- src/Tests/BusinessDaysTest.php
+++ src/Tests/BusinessDaysTest.php
@@ -27,80 +27,62 @@ class BusinessDaysTest extends \PHPUnit_Framework_TestCase
 
     public function testCanGetTwoDaysDifference()
     {
-        $wd = $this->dt->getBusinessDays('11/10/2014','11/12/2014');
+        $wd = $this->dt->getBusinessDays('11/10/2014', '11/12/2014');
         $this->assertSame(2, $wd);
     }
 
     public function testCanGetWeekAndAHalfWorkingDaysDifference()
     {
-        $wd = $this->dt->getBusinessDays('11/6/2014','11/18/2014');
+        $wd = $this->dt->getBusinessDays('11/6/2014', '11/18/2014');
         $this->assertSame(8, $wd);
     }
 
     public function testCanRemoveWeekendInBetween()
     {
-        $wd = $this->dt->getBusinessDays('11/13/2014','11/17/2014');
+        $wd = $this->dt->getBusinessDays('11/13/2014', '11/17/2014');
         $this->assertSame(2, $wd);
     }
 
     public function testCanRemoveSevenDayWeek()
     {
-        $wd = $this->dt->getBusinessDays('11/01/2014','11/08/2014');
+        $wd = $this->dt->getBusinessDays('11/01/2014', '11/08/2014');
         $this->assertSame(5, $wd);
     }
 
     public function testCanGet5BusDaysFromSunToSatPeriod()
     {
-        $wd = $this->dt->getBusinessDays('11/02/2014','11/08/2014');
+        $wd = $this->dt->getBusinessDays('11/02/2014', '11/08/2014');
         $this->assertSame(5, $wd);
     }
 
     public function testCanGet2WeeksworkingDays()
     {
-        $wd = $this->dt->getBusinessDays('11/09/2014','11/22/2014');
+        $wd = $this->dt->getBusinessDays('11/09/2014', '11/22/2014');
         $this->assertSame(10, $wd);
     }
 
     public function testCanCalculateAllWorkingDaysInMonth()
     {
-        $wd = $this->dt->getBusinessDays('11/01/2014','11/30/2014');
+        $wd = $this->dt->getBusinessDays('11/01/2014', '11/30/2014');
         $this->assertSame(20, $wd);
     }
 
     public function testCanCalculateWorkingDaysDistanceInNonWeekendStartingMonth()
     {
-        $wd = $this->dt->getBusinessDays('10/01/2014','10/31/2014');
+        $wd = $this->dt->getBusinessDays('10/01/2014', '10/31/2014');
         $this->assertSame(22, $wd);
     }
 
     public function testCanCalculateWorkingDaysDistanceIn2Months()
     {
-        $wd = $this->dt->getBusinessDays('11/01/2014','12/31/2014');
+        $wd = $this->dt->getBusinessDays('11/01/2014', '12/31/2014');
         $this->assertSame(42, $wd);
-    }
-
-    public function testCanFlagWithinThreeDaysFromNow()
-    {
-        $now_obj = new \DateTime('now');
-        $now_obj->add(new \DateInterval('P2D'));
-        $soon = (string)$now_obj->format('m/d/Y');
-        $wd = $this->dt->getBusinessDaysFromNow($soon);
-        $this->assertSame(2,$wd);
     }
 
     public function testCanGetDiffInDays()
     {
         $d = $this->dt->getRawDiffInDays('11/01/2014', '11/3/2014');
-        $this->assertSame(2,$d);
-    }
-
-    public function testCanGetDiffInDaysFromNow()
-    {
-        $target = new \DateTime('now');
-        $target->add(new \DateInterval('P2D'));
-        $target = $target->format('m/d/Y');
-        $d = $this->dt->getBusinessDaysFromNow($target);
-        $this->assertSame(2,$d);
+        $this->assertSame(2, $d);
     }
 
     public function testCanGetThreeBusinessDaysOut()
